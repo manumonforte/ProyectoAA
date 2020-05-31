2ee7701c7db24f9c0a2e18ecbf998daea47cd81a
Manuel Monforte <manumonforesco2@gmail.com>
2020-05-31 19:54:58 +0200

    add class to linear reg

--- LinearRegression.py
+++ LinearRegression.py
@@ -9,27 +9,30 @@ class OurLinearRegression:
         self.num_features = num_features
         self.thetas = np.zeros(self.num_features)
 
-    def cost(self, X, Y, reg=0):
+    def cost(self, thetas, X, Y, reg=0):
         m = X.shape[0]
-        H = np.dot(X, self.thetas)
-        cost = (1/(2*m)) * np.sum((H-Y.T)**2) + ( reg / (2 * m) ) * np.sum(self.thetas[1:]**2)
+        H = np.dot(X, thetas)
+        cost = (1/(2*m)) * np.sum((H-Y.T)**2) + ( reg / (2 * m) ) * np.sum(thetas[1:]**2)
         return cost
-
-    def gradient(self, X, Y, reg=0):
-        tt = np.copy(self.thetas)
+    
+    def gradient(self, thetas, X, Y, reg=0):
+        tt = np.copy(thetas)
         tt[0]=0
         m = X.shape[0]
-        H = np.dot(X, self.thetas)
-        gradient = ((1 / m) * np.dot(H-Y.T,X.T)) + ((reg/m) * tt)
+        H = np.dot(X, thetas)
+        gradient = ((1 / m) * np.dot(H-Y.T,X)) + ((reg/m) * tt)
         return gradient
 
-    def linearCostGrad(self,X,Y,reg=0):
-        return (self.cost(X,Y,reg),self.gradient(X,Y).flatten())
+    def linearCostGrad(self, thetas, X, Y, reg=0):
+        return (self.cost(thetas,X,Y,reg),self.gradient(thetas,X,Y).flatten())
 
     def train(self, X, Y):
-        initial_thetas = np.zeros(X.shape[1])
-        return opt.minimize(fun=self.linearCostGrad,
+        initial_thetas = np.zeros(self.num_features)
+        self.thetas = opt.minimize(fun=self.linearCostGrad,
                        x0=initial_thetas,
-                       args=(X,Y),
+                       args=(X,Y,self.reg),
                        method='TNC',
                        jac=True).x
+
+    def predict(self, X):
+        return X @ self.thetas
